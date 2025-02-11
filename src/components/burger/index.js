d1f4ff13ff5c0b0e1f59b2522546caac5bbebcd9
Munkhherdene <boy76428@gmail.com>
2025-02-11 14:52:22 +0800

    burger

--- src/components/burger/index.js
+++ src/components/burger/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import BurgerIngredient from "../burgerIngredient";
+import css from "./style.module.css";
 
 const Burger = () => {
     return ( 
-        <div>
+        <div className={css.Burger}>
         <BurgerIngredient type="breadTop"/>
         <BurgerIngredient type="salad"/>
         <BurgerIngredient type="bacon"/>
         <BurgerIngredient type="cheese"/>
         <BurgerIngredient type="meat"/>
         <BurgerIngredient type="meat"/>
+        <BurgerIngredient type="breadBottom"/>
         </div>
     )
 }
--- src/components/burgerIngredient/index.js
+++ src/components/burgerIngredient/index.js
@@ -1,8 +1,26 @@
 import React from "react";
-
+import css from "./style.module.css";
 
 const BurgerIngredient = (props) => {
-    return <div>{props.type}</div>;
-}
+    if(props.type == 'salad')
+        return <div className={css.Salad}>{props.type}</div>; 
+    if (props.type == 'bacon')
+        return <div className={css.Bacon}>{props.type}</div>; 
+    if (props.type == 'meat')
+        return <div className={css.Meat}>{props.type}</div>; 
+    if (props.type == 'cheese')
+        return <div className={css.Cheese}>{props.type}</div>; 
+    if (props.type == 'breadTop')
+        return(
+        <div className={css.BreadTop}> 
+        <div className={css.Seed}></div>
+        <div className={css.Seed}></div>
+        <div className={css.Seed}></div>
+        <div className={css.Seed}></div>    
+        </div>
+        );
+    if (props.type == 'breadBottom')
+        return <div className={css.BreadBottom}>{props.type}</div>; 
+}   
 
 export default BurgerIngredient;
